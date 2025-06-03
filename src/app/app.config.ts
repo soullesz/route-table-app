114161165ac1ed9b642921d939f58c23e774e3fb
Artem <plikban@gmail.com>
2025-06-03 18:07:44 +0300

    add ApplicationConfig for standalone app (appConfig)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes'; 
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+
+import type { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    importProvidersFrom(
+      BrowserModule,
+      MatTableModule,
+      MatSortModule,
+      MatIconModule
+    ),
+    provideAnimations(),
+    provideRouter(appRoutes) 
   ]
 };
