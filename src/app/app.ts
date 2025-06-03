baa4771b86d361c062d381b82bb9a8f1b41f5324
Artem <plikban@gmail.com>
2025-06-03 18:00:53 +0300

    configure App as standalone and import RouteTableComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouteTableComponent } from './components/route-table/route-table';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app',
+  standalone: true,
+  imports: [RouteTableComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected title = 'route-table-app';
-}
+export class App {}
