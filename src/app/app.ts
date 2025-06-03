9c62d993e4de4aa14fadf6b5c97edac07e5721d0
Artem <plikban@gmail.com>
2025-06-03 18:46:01 +0300

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
+
 import { Component } from '@angular/core';
 import { RouteTableComponent } from './components/route-table/route-table';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [RouteTableComponent],
+  imports: [
+    RouteTableComponent,
+    RouterOutlet,        
+    RouterLink,          
+    RouterLinkActive     
+  ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
